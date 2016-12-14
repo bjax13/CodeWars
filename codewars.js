453d0b6b3cb6cb6321fafaed67627587890ab353
Bryan Jackson <bjack13@gmail.com>
2016-12-14 09:27:07 -0700

    change to piglatin challange

--- codewars.js
+++ codewars.js
@@ -1,1 +1,20 @@
 
+function pigIfy(string) {
+  var arr = string.split(" ")
+
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(1)+ arr[i].charAt(0) + "a"
+  }
+  console.log(arr);
+  return arr.join(' ');
+}
+
+console.log(pigIfy('translate english to pig latin'));
+
+
+function pigIfy2(string) {
+
+  return string.split('').forEach(function (word) {word = word.slice(1) + word.charAt(0) + "a"}).join(" ")
+}
+
+console.log(pigIfy2('translate english to pig latin'));
