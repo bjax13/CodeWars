3436dab61665889b4d6171bb582ec13f5068579c
Bryan Jackson <bjack13@gmail.com>
2016-12-15 11:29:28 -0700

    first pass on submission 2 errors

--- codewars.js
+++ codewars.js
@@ -1,3 +1,35 @@
 function primeFactors(n){
     //your code here
+    var string = "";
+
+    function isPrime(num) {
+      for (var i = 2; i * i <= num; i++) {
+        if (num % i) {
+          return  false;
+        }
+      }
+      return true;
+    }
+
+    for (var i = 2; i <= n; i++) {
+      var count = 0;
+
+
+        while (n%i===0) {
+          count++;
+          n = n/i;
+        }
+        if (count === 0) {
+
+        }else if (count === 1) {
+          string+= "("+i+")";
+        }else {
+          string+=("("+i+"**"+count+")");
+        }
+
+    }
+
+    return string;
 }
+
+console.log(primeFactors(86240));
