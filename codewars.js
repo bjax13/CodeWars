24ed1780872d7cbfc917a2842c7ab4a4702775fc
Bryan Jackson <bjack13@gmail.com>
2016-12-17 10:41:34 -0700

    first pass 7 errors remaining.

--- codewars.js
+++ codewars.js
@@ -1,3 +1,48 @@
 function decompose(n) {
-    // your code
+    if (n<5) {
+      return null;
+    }
+
+
+
+    var remainder = 0;
+    var arr = [];
+
+    var nSquared = n * n - ((n-1)*(n-1));
+
+    arr.push(n-1);
+
+    while (nSquared > 0) {
+      var root  = Math.floor(Math.sqrt(nSquared),1)
+      nSquared-=root*root;
+      arr.unshift(root);
+    }
+
+
+    return arr;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(decompose(2));
+console.log(decompose(7));
