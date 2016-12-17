518629e6d0b613f7db2f71838ad8be8c5b259c42
Bryan Jackson <bjack13@gmail.com>
2016-12-17 13:36:05 -0700

    progress on problem

--- codewars.js
+++ codewars.js
@@ -8,20 +8,39 @@ function decompose(n) {
     var remainder = 0;
     var arr = [];
 
-    var nSquared = n * n - ((n-1)*(n-1));
+    var nSquared = n * n;
 
-    arr.push(n-1);
 
-    var arr2 = sumsOfSq(nSquared);
 
-    for (var i = 0; i < arr2.length; i++) {
-      arr.unshift(arr2[i]);
+    var arr2 = sumsOfSq(nSquared,n-1);
+
+    if (passed(arr2)) {
+      for (var i = 0; i < arr2.length; i++) {
+        arr.unshift(arr2[i]);
+      }
+    }else {
+      arr2.reverse();
+      var smallTot = 0
+      for (var i = 0; i < arr2.length; i++) {
+        smallTot+=arr2[i]*arr2[i];
+        if (arr2[i]<5) {
+          arr2.shift();
+          i--;
+        }else {
+          var arr3 = sumsOfSq(smallTot,arr2[i]-1)
+          console.log(arr3);
+        }
+      }
     }
 
 
-    function sumsOfSq(nSquared) {
+
+
+    function sumsOfSq(nSquared,startInt) {
       var total = nSquared;
       var arr2 =[];
+      total-= startInt*startInt;
+      arr2.push(startInt)
       while (total > 0) {
         var root  = Math.floor(Math.sqrt(total),1)
         total-=root*root;
