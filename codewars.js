8b8c46889d5f899aad05da0ee3f34df1f17dca5a
Bryan Jackson <bjack13@gmail.com>
2016-12-18 11:48:06 -0700

    first pass at problem

--- codewars.js
+++ codewars.js
@@ -1,9 +1,19 @@
 var nbrOfLaps = function (x, y) {
-  var xCount = 0;
-  var yCount = 0;
+  var xCount = 1;
+  var yCount = 1;
   var xTot = x;
   var yTot = y;
 
+  while (xTot !== yTot) {
+    if (xTot> yTot) {
+      yTot+=y;
+      yTot++;
+    }else {
+      xTot+=x;
+      xTot++;
+    }
+  }
 
-  return [0, 0];
+
+  return [xCount, yCount];
 }
