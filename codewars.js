e79eb7cbe55bb71a71d86d2294ce1feec2e27ba0
Bryan Jackson <bjack13@gmail.com>
2016-12-18 12:24:43 -0700

    determined that data passed into function is an array

--- codewars.js
+++ codewars.js
@@ -1,2 +1,16 @@
-function lastDigit(as){
+function lastDigit(array){
+var argArray = array;
+
+// establish power pattern for first argument
+// Link -> http://mathcentral.uregina.ca/qq/database/qq.09.04/landon1.html
+// the link explains a pattern for powers.
+
+
+
+
+console.log(argArray);
+
+
 }
+
+console.log(lastDigit([ 4, 3, 6 ]));
