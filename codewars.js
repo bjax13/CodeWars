a5384024dec80e597e92586bb9d88fce4967e9bb
Bryan Jackson <bjack13@gmail.com>
2016-12-19 09:05:27 -0700

    morning update

--- codewars.js
+++ codewars.js
@@ -1,5 +1,6 @@
 function lastDigit(array){
 var argArray = array;
+var powerPattern = [];
 
 // establish power pattern for first argument
 // Link -> http://mathcentral.uregina.ca/qq/database/qq.09.04/landon1.html
@@ -10,7 +11,6 @@ var argArray = array;
 
 console.log(argArray);
 
-
 }
-
+// 4
 console.log(lastDigit([ 4, 3, 6 ]));
