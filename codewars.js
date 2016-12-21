72055c6ac6801d2a4d9cacb5a418ec823e0c27f0
Bryan Jackson <bjack13@gmail.com>
2016-12-21 09:53:38 -0700

    first pass at function

--- codewars.js
+++ codewars.js
@@ -2,6 +2,7 @@ function lastDigit(array) {
     console.log(array);
     var argArray = array;
 
+
     console.log(Math.pow(1, 0));
     // test if array is empty
     if (argArray.length === 0) {
@@ -10,6 +11,12 @@ function lastDigit(array) {
 
     argArray[0] = lastDigit(argArray[0]);
 
+    // test if array has length of 1 return last digit if so
+    if (argArray.length === 1) {
+      return argArray[0]
+    }
+
+    // arguments starting with 0 testcase;
     if (argArray[0] === 0) {
         var total = 0;
         for (var i = 1; i < argArray.length; i++) {
@@ -23,6 +30,23 @@ function lastDigit(array) {
         return total;
     }
 
+    var lastDigitPattern = [];
+
+
+
+    var digit = argArray[0];
+    var mod = 0;
+
+    for (var i = 0; i < argArray.length-1; i++) {
+      lastDigitPattern = powerPattern(digit);
+
+      mod = argArray[i+1] % lastDigitPattern.length;
+
+      digit = lastDigitPattern[mod];
+
+    }
+
+    return digit
     // establish power pattern for first argument
     // Link -> http://mathcentral.uregina.ca/qq/database/qq.09.04/landon1.html
     // Link -> https://brilliant.org/wiki/finding-the-last-digit-of-a-power/
