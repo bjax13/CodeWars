18a5cfdde66fd836584395b80d59a892e757523e
Bryan Jackson <bjack13@gmail.com>
2016-12-22 09:54:37 -0700

    works for all edge cases.  still not 100%

--- codewars.js
+++ codewars.js
@@ -2,8 +2,7 @@ function lastDigit(array) {
     console.log(array);
     var argArray = array;
 
-    console.log(Math.pow(4,2));
-    console.log(Math.pow(4,729));
+
 
     // test if array is empty
     if (argArray.length === 0) {
@@ -31,45 +30,88 @@ function lastDigit(array) {
         return total;
     }
 
+    // arguments with 1 in power positions return last digit of first argument if so
 
-
-
+    for (var i = 1; i < argArray.length; i++) {
+      if (argArray[i]===1) {
+        return argArray[0]
+      }else if (argArray[i]===0) {
+        return 1;
+      }
+    }
 
     var digit = argArray[0];
     var mod = 0;
     var lastDigitPattern = powerPattern(digit);
 
-    if (argArray[0] > argArray[1]) {
-      lastDigitPattern.reverse();
-      for (var i = 1; i < argArray.length; i++) {
-        argArray[i] = lastDigitPattern[argArray[i]%powerPattern.length];
-      }
+    if (digit===1 || digit===5 ||digit===6) {
+      return digit;
+    }
+
+    // reduce each to last digit of each power
+
+    for (var i = argArray.length-1; i >=1 ; i--) {
+      argArray[i] = argArray[i]% lastDigitPattern.length
+
+      argArray[i] = lastDigitPattern[argArray[i]];
+
+      console.log(argArray);
+    }
+
+    var total;
+
+    for (var i = argArray.length-1; i >=1 ; i--) {
+      total = Math.pow(argArray[i-1],argArray[i])
+
+      argArray[i-1]= total% lastDigitPattern.length
+
+      argArray[i-1] = lastDigitPattern[argArray[i-1]];
+
+      console.log(argArray);
+    }
+
+    if (argArray[1]%lastDigitPattern.length-1 <0) {
+      return lastDigitPattern[lastDigitPattern.length-1]
+    }else {
+      return lastDigitPattern[argArray[1]%lastDigitPattern.length-1]
     }
 
 
-    console.log(argArray);
 
 
 
-    // works for most but not for some.
 
-    for (var i = 0; i < argArray.length-1; i++) {
+    //
+    // if (argArray[0] > argArray[1]) {
+    //   lastDigitPattern.reverse();
+    //   for (var i = 1; i < argArray.length; i++) {
+    //     argArray[i] = lastDigit(argArray[i])
+    //   }
+    // }
+    //
+    //
+    // console.log(argArray);
+    //
+    //
+    //
+    // // works for most but not for some.
+    //
+    // for (var i = 0; i < argArray.length-1; i++) {
+    //
+    //
+    //   mod = argArray[i+1] % lastDigitPattern.length;
+    //
+    //   if (!mod) {
+    //     mod = lastDigitPattern.length;
+    //   }
+    //
+    //   digit = lastDigitPattern[mod-1];
+    //
+    // }
+    //
+    // return digit
 
-      console.log();
-      mod = argArray[i+1] % lastDigitPattern.length;
-      console.log(digit);
-      // if (argArray[i] > argArray[i+1]) {
-      //   lastDigitPattern.reverse();
-      // }
-      if (!mod) {
-        mod = lastDigitPattern.length;
-      }
 
-      digit = lastDigitPattern[mod-1];
-
-    }
-
-    return digit
     // establish power pattern for first argument
     // Link -> http://mathcentral.uregina.ca/qq/database/qq.09.04/landon1.html
     // Link -> https://brilliant.org/wiki/finding-the-last-digit-of-a-power/
@@ -86,7 +128,7 @@ function lastDigit(array) {
             }
             pattern.push(last)
         }
-        return pattern.reverse();
+        return pattern;
     }
 
 
@@ -101,6 +143,9 @@ function lastDigit(array) {
     console.log(argArray);
 
 }
-// 4
+// 1
 
+console.log(lastDigit([7,6,21]));
+
+//4
 console.log(lastDigit([4,3,6]));
