a4f62cd5638e6b68a43e25c0ee92d2c717830aa4
Bryan Jackson <bjack13@gmail.com>
2016-12-24 14:13:51 -0700

    looked over others solutions added 2 that were interesting

--- Kata3/digitOfLgNum.js
+++ Kata3/digitOfLgNum.js
@@ -14,10 +14,10 @@
 //
 // This kata generalizes Last digit of a large number; you may find useful to solve it beforehand.
 
-//   My solution 
+//   My solution
 
 function lastDigit(array) {
-    console.log(array);
+
     var argArray = array;
 
     // test if array is empty
@@ -29,7 +29,7 @@ function lastDigit(array) {
 
     // test if array has length of 1 return last digit
     if (argArray.length === 1) {
-      return argArray[0]
+        return argArray[0]
     }
 
     // arguments starting with 0 testcase;
@@ -49,52 +49,47 @@ function lastDigit(array) {
     // arguments with 1 in power positions return last digit of first argument if so
 
     for (var i = 1; i < argArray.length; i++) {
-      if (argArray[i]===1) {
-        return argArray[0]
-      }else if (argArray[i]===0) {
-        return 1;
-      }
+        if (argArray[i] === 1) {
+            return argArray[0]
+        } else if (argArray[i] === 0) {
+            return 1;
+        }
     }
 
     var digit = argArray[0];
 
     // since the powers of 1 5 and 6 always end in 1 5 or 6  quickly return befor doing calculations
-    if (digit===1 || digit===5 ||digit===6) {
-      return digit;
+    if (digit === 1 || digit === 5 || digit === 6) {
+        return digit;
     }
 
 
     // test if only 2 args in array if so use simple function
     if (argArray.length === 2) {
-      return lastOf2(argArray[0], (argArray[1]%100))
+        return lastOf2(argArray[0], (argArray[1] % 100))
     }
 
     // uses if array has 3 + items in array
 
     // for powers of powers return last 2 digits then once on first arg return last digit
 
-    for (var i = argArray.length-2; i >= 0; i--) {
-      if (i===0) {
-        if (argArray[i+1].length >99) {
-          argArray[i] = lastOf2((lastDigit(argArray[i])), last2Digit(argArray[i+1]))
-        }else {
-          argArray[i] = lastOf2((lastDigit(argArray[i])), (argArray[i+1]))
+    for (var i = argArray.length - 2; i >= 0; i--) {
+        if (i === 0) {
+            if (argArray[i + 1].length > 99) {
+                argArray[i] = lastOf2((lastDigit(argArray[i])), last2Digit(argArray[i + 1]))
+            } else {
+                argArray[i] = lastOf2((lastDigit(argArray[i])), (argArray[i + 1]))
+            }
+        } else {
+            if (argArray[i + 1].length > 99) {
+                argArray[i] = last2Of2((lastDigit(argArray[i])), last2Digit(argArray[i + 1]))
+            } else {
+                argArray[i] = last2Of2((lastDigit(argArray[i])), (argArray[i + 1]))
+            }
         }
-      }else {
-        if (argArray[i+1].length >99) {
-          argArray[i] = last2Of2((lastDigit(argArray[i])), last2Digit(argArray[i+1]))
-        }else {
-          argArray[i] = last2Of2((lastDigit(argArray[i])), (argArray[i+1]))
-        }
-      }
-
-
-
     }
 
     return argArray[0]
-
-
 
     // finding the last digits is best accomplished by using a pattern links below explain more
     // Link -> http://mathcentral.uregina.ca/qq/database/qq.09.04/landon1.html
@@ -103,11 +98,11 @@ function lastDigit(array) {
 
     //function uses shortcuts as noted in links above to get last digit with 2 args
     function lastOf2(num1, num2) {
-      return Math.pow(num1, (num2 % 4) || 4) %10;
+        return Math.pow(num1, (num2 % 4) || 4) % 10;
     }
     //function uses shortcuts as noted in links above to get last 2 digits
     function last2Of2(num1, num2) {
-      return Math.pow(num1, (num2 % 4) + 4) %100;
+        return Math.pow(num1, (num2 % 4) + 4) % 100;
     }
 
     //func to find last digit of a number
@@ -118,12 +113,10 @@ function lastDigit(array) {
     function last2Digit(num) {
         return parseInt(num.toString()[num.toString().length - 2])
     }
-
-
 }
 // 1
 
-console.log(lastDigit([7,6,21]));
+console.log(lastDigit([7, 6, 21]));
 
 //4
-console.log(lastDigit([4,3,6]));
+console.log(lastDigit([4, 3, 6]));
