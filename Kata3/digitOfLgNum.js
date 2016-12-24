ef5fbbe5d7d56f71643f391b245c7aa5c79b5f6f
Bryan Jackson <bjack13@gmail.com>
2016-12-24 14:18:44 -0700

    ready for next kata

--- Kata3/digitOfLgNum.js
+++ Kata3/digitOfLgNum.js
@@ -114,6 +114,92 @@ function lastDigit(array) {
         return parseInt(num.toString()[num.toString().length - 2])
     }
 }
+
+// others solutions from CodeWars
+
+
+// this one I liked because it was clever and short.  having never used the reduceRight function before it was interesting to see it in practice
+
+// function lastDigit(as){
+//   return as.reduceRight(function(pow, base, index, array) {
+//     return pow === 0 ? 1 : ( +pow == 1 ? base : Math.pow(base % 100, +pow % 4 + 4));
+//   }, 1) % 10;
+// }
+
+// this is what I felt is best practice.  easier to read and follow than the last yet still very fast.
+
+// function lastDigit(as) {
+//   /** HANDLE SPECIAL CASES **/
+//   // reduce zeros ( 0^n = 0, n^0 = 1)
+//   // removes all elements after the digit before the last zero
+//   // and iterates while zeros are found
+//   do {
+//     var zeroInd = as.lastIndexOf(0);
+//     if (zeroInd > 0 ) as.splice(zeroInd - 1);
+//   } while (zeroInd > 0)
+//
+//   if (as.length === 0) {
+//     // empty array returns 1
+//     return 1;
+//   } else if (as.length === 1) {
+//     // if there is only one element, return its last digit
+//     return as[0] % 10;
+//   }
+//   // 0^n = 0
+//   if (as[0] === 0) return 0;
+//
+//   var baseDigit = as[0] % 10;
+//   // if k % 10 = 0|1|5|6, last digit is always 0|1|5|6
+//   if ( baseDigit === 0 || baseDigit === 1 ||
+//     baseDigit === 5 || baseDigit === 6) return baseDigit;
+//
+//   /** SPECIAL CASES DONE **/
+//
+//   // n^k last digit has a cyclic behavior
+//   var map = {
+//     2: [6, 2, 4, 8],
+//     3: [1, 3, 9, 7],
+//     4: [6, 4, 6, 4],
+//     7: [1, 7, 9, 3],
+//     8: [6, 8, 4, 2],
+//     9: [1, 9, 1, 9]
+//   }
+//
+//   // we have to calculate the % 4 of the total exponent
+//   var baseExp = as[1] % 4;
+//
+//   switch (baseExp) {
+//     case 0:
+//     case 1:
+//       // if the base exponent is (4k + 0|1),
+//       // (4k + 0|1)^n % 4 = 0|1 for every n
+//       return map[baseDigit][baseExp]
+//     case 2:
+//       // if baseExp is 4k + 2,
+//       // (4k + 2)^n % 4 = 0 if n > 1
+//       if (as[2] && as[2] > 1) {
+//         return map[baseDigit][0];
+//       } else {
+//         // (4k + 2)^n % 4 = 2 if n = 1
+//         return map[baseDigit][2];
+//       }
+//     case 3:
+//       if (as[2] && as[2] % 2 === 0) {
+//         // if baseExp = 4k + 3,
+//         // (4k + 3)^n % 4 = 1 if n is even,
+//         return map[baseDigit][1];
+//       } else {
+//         // (4k + 3)^n % 4 = 3 if n is odd
+//         // see https://en.wikipedia.org/wiki/Euler%27s_theorem,
+//         // using phi(4) = 2 => 3^(2k + 0|1) = 1|3 mod (4)
+//         return map[baseDigit][3];
+//       }
+//     default:
+//       return null;
+//   }
+// }
+
+
 // 1
 
 console.log(lastDigit([7, 6, 21]));
