2313140ab2c9ee3a38a6756b644a407efbce3d80
Bryan Jackson <bjack13@gmail.com>
2016-12-26 23:27:38 -0700

    trying to get to fail when non alphanumeric chars are used

--- Kata5/regxPassVerify.js
+++ Kata5/regxPassVerify.js
@@ -3,7 +3,12 @@
 //   return /"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"/.test(password);
 // }
 
+// function validate(password) {
+//   console.log(password);
+//   return /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"/.test(password);
+// }
+
 function validate(password) {
   console.log(password);
-  return /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"/.test(password);
+  return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/.test(password);
 }
