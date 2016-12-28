ce91ecbb9fe37c39d6d0c8e4a57a8f6431cc70df
Bryan Jackson <bjack13@gmail.com>
2016-12-28 10:52:34 -0700

    solved for 3 working on veriable length

--- Kata5/bestTrav.js
+++ Kata5/bestTrav.js
@@ -1,3 +1,34 @@
 function chooseBestSum(t, k, ls) {
+  console.log("t = "+ t);
+  console.log("k = "+ k);
+  console.log("ls = "+ ls);
+
+  if (ls.length < 3) {
+    return null
+  }
+
+  let longest = 0;
+
+  for (var i = 0; i < ls.length-2; i++) {
+    for (var j = i+1; j < ls.length-1; j++) {
+      for (var l = j+1; l < ls.length; l++) {
+        if (ls[i]+ls[j]+ls[l] > longest && ls[i]+ls[j]+ls[l] <= t) {
+          longest = ls[i]+ls[j]+ls[l];
+          console.log("long = "+longest);
+          console.log("i = "+ls[i]);
+          console.log("j = "+ls[j]);
+          console.log("l = "+ls[l]);
+          if (longest === t) {
+            return longest
+          }
+        }
+      }
+    }
+  }
+  return longest
+
+
     // your code
 }
+
+console.log(chooseBestSum(230, 3, [91,74,73,85,73,81,87]));
