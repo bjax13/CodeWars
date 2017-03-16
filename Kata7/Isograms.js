e731da6a797f8532a08c0fe921b19884e8d77da5
Bryan Jackson <bjack13@gmail.com>
2017-03-16 16:04:16 -0600

    first attempt

--- Kata7/Isograms.js
+++ Kata7/Isograms.js
@@ -1,3 +1,12 @@
 function isIsogram(str){
-  //...
+  let arrStr = str.toLowerCase().split('');
+  let lettersUsed = [];
+  for (var i = 0; i < arrStr.length; i++) {
+    if (lettersUsed.indexOf(arrStr[i])=== -1) {
+      lettersUsed.push(arrStr[i]);
+    }else {
+      return false;
+    }
+  }
+  return true;
 }
