b0e5b6c9a354fbaa2bc246e972a00f7bf710c51e
Bryan Jackson <bjack13@gmail.com>
2017-03-17 13:35:13 -0600

    first attempt

--- Kata4/SudokuNxN.js
+++ Kata4/SudokuNxN.js
@@ -1,17 +1,37 @@
 var Sudoku = function(data)
 {
+  var valid = false;
   //   Private methods
   // -------------------------
+  function validSolution(board){
+    var sumh = [0,0,0,0,0,0,0,0,0];
+    var sumv = [0,0,0,0,0,0,0,0,0];
+    osums = [[0,0,0],[0,0,0],[0,0,0]];
+    for (var i=0;i<9;i++){
+      for (var j=0;j<9;j++){
+        sumh[i] += board[i][j];
+        sumv[j] += board[i][j];
+        osums[Math.floor(i/3)][Math.floor(j/3)] += board[i][j];
+      }
+    }
+    for (var i=0;i<3;i++) if (!osums[i].every(equals45)) return false;
+    return (sumh.every(equals45) && sumv.every(equals45));
+  }
 
+  if (validSolution(data)){
+    valid = true;
+  }
 
 
   //   Public methods
+  function equals45(n){
+    return n == 45;
+  }
   // -------------------------
   return {
     isValid: function() {
-      // YOUR SOLUTION
 
-      return true;
+      return valid;
     }
   };
 };
--- Kata4/sudokuValidator.js
+++ Kata4/sudokuValidator.js
@@ -59,6 +59,8 @@ function validSolution(board){
   return (testColumns(board) * testRows(board) * testBlocks(board))===1;
 }
 
+
+
 console.log(validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
                            [6, 7, 2, 1, 9, 5, 3, 4, 8],
                            [1, 9, 8, 3, 4, 2, 5, 6, 7],
