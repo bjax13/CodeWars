837ebcbfebb67c0f18c04603a06f45e2588262ef
Bryan Jackson <bjack13@gmail.com>
2017-03-22 10:33:39 -0600

    adding function to test if solved.

--- Kata3/sudokuSolver.js
+++ Kata3/sudokuSolver.js
@@ -4,8 +4,33 @@ function sudoku(puzzle) {
   var solvedPuzzle = puzzle;
 
 
+  function equals45(n){
+    return n == 45;
+  }
+
+  function validSolution(board){
+    var sumh  = [0,0,0,0,0,0,0,0,0];
+    var sumv  = [0,0,0,0,0,0,0,0,0];
+    var osums = [[0,0,0],[0,0,0],[0,0,0]];
+    for (var i=0;i<9;i++){
+      for (var j=0;j<9;j++){
+        sumh[i] += board[i][j];
+        sumv[j] += board[i][j];
+        osums[Math.floor(i/3)][Math.floor(j/3)] += board[i][j];
+      }
+    }
+    for (var i=0;i<3;i++) if (!osums[i].every(equals45)) return false;
+    return (sumh.every(equals45) && sumv.every(equals45));
+  }
+
   for (var i = 0; i < solvedPuzzle.length; i++) {
-    solvedPuzzle[i]
+    for (var j = 0; j < solvedPuzzle.length; j++) {
+      solvedPuzzle[i][j]
+
+
+
+
+    }
   }
 
 
