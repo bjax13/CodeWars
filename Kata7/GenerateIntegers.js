ae7a2c3526406abc2a8cc3189d0614ab49ec182e
Bryan Jackson <bjack13@gmail.com>
2017-03-24 20:51:37 -0600

    kata 6 finished for comp

--- Kata7/GenerateIntegers.js
+++ Kata7/GenerateIntegers.js
@@ -1,13 +1,5 @@
-function openOrSenior(data){
-
-  var arr = [];
-
-  for (var i = 0; i < data.length; i++) {
-    if (data[i][0]>= 55 && (data[i][1] > 7 && data[i][1] < 27)) {
-      arr.push('Senior');
-    }else {
-      arr.push('Open');
-    }
+function compose(f,g) {
+  return function (x) {
+    return f(g(x))
   }
-  return arr;
 }
