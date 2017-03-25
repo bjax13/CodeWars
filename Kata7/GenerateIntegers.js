785b5a3422504f7d2ef92432cd958eb82f89870e
Bryan Jackson <bjack13@gmail.com>
2017-03-24 19:57:58 -0600

    Kata 7 finished

--- Kata7/GenerateIntegers.js
+++ Kata7/GenerateIntegers.js
@@ -1,7 +1,19 @@
-function generateRange(min, max, step){
-  let arr = [];
-  for (let i=min; i<=max; i += step) {
-    arr.push(i);
+function longest(s1, s2) {
+  console.log(s1);
+  console.log(s2);
+  let arr1 = s1.split('');
+  let arr2 = s2.split('');
+  let uniq = [];
+
+  arr1.concat(arr2)
+
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (uniq.indexOf(arr1[i])=== -1) {
+      uniq.push(arr1[i])
+    }
   }
-  return arr;
+
+  return uniq.join('');
+
 }
