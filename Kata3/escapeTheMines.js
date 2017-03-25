08f0bd9ed8848002b8abab9733431ab221c1ea5e
Bryan Jackson <bjack13@gmail.com>
2017-03-25 01:08:24 -0600

    test added

--- Kata3/escapeTheMines.js
+++ Kata3/escapeTheMines.js
@@ -1,9 +1,22 @@
 function solve(map, miner, exit) {
-  // TODO
-  return [];
+  var maxX = map[0].length;
+  var maxY = map.length;
+  var instruction = [];
+
+  for (var i = miner.y; i < map.length; i++) {
+    for (var j = miner.x; j < map[i].length; j++) {
+      if (i === exit.y && j === exit.x) {
+        return instruction;
+      }
+
+    }
+  }
+
+
+  return ['default'];
 }
 
 var map = [[true, false],
            [true, true]];
 
-console.log(map, {x:0,y:0}, {x:1,y:1});
+console.log(solve(map, {x:0,y:0}, {x:1,y:1}));
