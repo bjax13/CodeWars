3f35624651d6e03908675817c4aa1a97fd54670d
Bryan Jackson <bjack13@gmail.com>
2017-03-29 14:21:08 -0600

    progress in kata

--- Kata4/romanNumeralsEncoder.js
+++ Kata4/romanNumeralsEncoder.js
@@ -1,5 +1,21 @@
 // https://www.codewars.com/kata/roman-numerals-encoder/train/javascript
 
 function solution(number){
-  // convert the number to a roman numeral
+
+  var arr = number.toString().split('');
+
+  var count = 0 ;
+  var numberString = '';
+  var solutionArray = [];
+
+  for (var i = arr.length-1; i > 0 ; i--) {
+
+    numberString = arr[i]
+
+    for (var i = 0; i < count; i++) {
+      numberString = numberString + '0';
+    }
+  }
+
+  return true;
 }
