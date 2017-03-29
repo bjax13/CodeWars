8bf75c4db282f0d986bf0f0d40b1b98770301133
Bryan Jackson <bjack13@gmail.com>
2017-03-29 13:15:40 -0600

    first attempt

--- Kata4/validBraces.js
+++ Kata4/validBraces.js
@@ -7,7 +7,15 @@ function validBraces(braces){
   var rightBraces = [')', '}', '] '];
 
   for (var i = 0; i < arr.length; i++) {
-    arr[i]
+    if (leftBraces.indexOf(arr[i])!= -1) {
+      leftStack.push(arr[i])
+    }else {
+      if (leftStack[leftStack.length-1] != leftBraces[rightBraces.indexOf(arr[i])]) {
+        return false;
+      }else {
+        leftStack.pop();
+      }
+    }
   }
 
   return true;
