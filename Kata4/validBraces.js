138fe55040fbaa8875edab9401e3ed108024da31
Bryan Jackson <bjack13@gmail.com>
2017-03-29 13:05:14 -0600

    set up frame of solution

--- Kata4/validBraces.js
+++ Kata4/validBraces.js
@@ -1,3 +1,14 @@
 function validBraces(braces){
-  //TODO
+  var arr = braces.split('');
+  var leftStack = [];
+  var rightStack = [];
+
+  var leftBraces = ['(', '{', '['];
+  var rightBraces = [')', '}', '] '];
+
+  for (var i = 0; i < arr.length; i++) {
+    arr[i]
+  }
+
+  return true;
 }
