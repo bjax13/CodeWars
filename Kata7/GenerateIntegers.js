7b27f92ea4e5d27de2288954a6bd49188bfb1d52
Bryan Jackson <bjack13@gmail.com>
2017-03-29 11:18:54 -0600

    find Even Index

--- Kata7/GenerateIntegers.js
+++ Kata7/GenerateIntegers.js
@@ -1,36 +1,21 @@
-function incrementString (strng) {
-  if (strng === '') {
-    return 1;
+function findEvenIndex(arr)
+{
+  if (arr.length ===0) {
+    return -1
   }
-  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    arr[i]
 
-  var arr = strng.split('')
+    if (arr.slice(0,i).reduce(function(acc, val) {
+        return acc + val;
+    }, 0) === arr.slice(i+1, slice.length-1).reduce(function(acc, val) {
+        return acc + val;
+    }, 0) ) {
+      return i;
+    }
 
-  if (parseInt(arr[arr.length-1])<9) {
-    arr[arr.length-1]++;
-  }else if (parseInt(arr[arr.length-1])===9) {
-    console.log('in');
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i]==='9') {
-        arr.splice(i,1)
-        i--;
-        count++;
-      }
-    }
-    if (parseInt(arr[arr.length-1])<9) {
-      arr[arr.length-1]++;
-    }else {
-      arr.push(1)
-    }
-    for (var i = 0; i < count; i++) {
-      arr.push('0')
-    }
+    
   }
-  else {
-    arr.push('1')
-  }
-  return arr.join('')
+
+  return -1
 }
-
-
-incrementString('foobar999')
