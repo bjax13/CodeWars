8fe56449addda44dc6ce9033473984dde9cbdcbe
Bryan Jackson <bjack13@gmail.com>
2017-06-21 11:56:55 -0600

    adding removeSuit and cardCount functions

--- Kata3/TexasHoldEmHands.js
+++ Kata3/TexasHoldEmHands.js
@@ -1,9 +1,50 @@
 function hand(holeCards, communityCards) {
-  console.log(holeCards);
-  console.log(communityCards);
+
+  let removeSuit = (array) =>{
+    return array.map((i)=> i[0])
+  }
+
+  let cardCount = (array) => {
+    var cardCountObj = array.reduce(function (acc, curr) {
+      if (typeof acc[curr] == 'undefined') {
+        acc[curr] = 1;
+      } else {
+        acc[curr] += 1;
+      }
+
+      return acc;
+    }, {});
+
+    return cardCountObj
+  }
 
 
+  let checkFourOfAKind = (hole , community) => {
+    let cards = hole.concat(community);
 
+    cards = removeSuit(cards)
+
+    var numCardsObj = cardCount(cards)
+
+    console.log(numCardsObj);
+
+    for (var rank in numCardsObj) {
+      if (numCardsObj.hasOwnProperty(rank)) {
+        if (numCardsObj[rank] === 4) {
+          console.log('true');
+          console.log(rank);
+          console.log(numCardsObj[rank]);
+        }else {
+          console.log(rank);
+          console.log(numCardsObj[rank]);
+        }
+
+      }
+    }
+
+  }
+
+  checkFourOfAKind(holeCards, communityCards)
 
 
   return {type:"TODO", ranks: []};
@@ -12,7 +53,10 @@ function hand(holeCards, communityCards) {
 
 
 //expected {"type":"nothing","ranks":["A","K","Q","J","9"]}
-console.log(hand([ 'K♠', 'A♦' ],[ 'J♣', 'Q♥', '9♥', '2♥', '3♦' ]));
+//console.log(hand([ 'K♠', 'A♦' ],[ 'J♣', 'Q♥', '9♥', '2♥', '3♦' ]));
 
 //expected {"type":"pair","ranks":["Q","K","J","9"]}
-console.log(hand([ 'K♠', 'Q♦' ]));
+//console.log(hand([ 'K♠', 'Q♦' ],[ 'J♣', 'Q♥', '9♥', '2♥', '3♦' ]));
+
+//expected {"type":"four-of-a-kind","ranks":["J","9"]}
+console.log(hand([ 'J♦', 'J♠' ],[ '9♦', '5♣', 'J♣', '4♦', 'J♥' ]));
